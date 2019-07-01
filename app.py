059a856fdc4671d8e95b95636682c16c5da2a5fb
escapist21 <ad.sourav21@live.com>
2019-07-01 18:52:00 +0530

    changed dictionary access method; added triples.json

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ import os
 from time import sleep
 from random import randint
 
-with open('District-Block_dictionary.json') as jsonfile:
-	district_dict = json.load(jsonfile)
+# with open('District-Block_dictionary.json') as jsonfile:
+# 	district_dict = json.load(jsonfile)
+#
+# with open('Block-Panchayat_dictionary.json') as jsonfile:
+# 	block_dict = json.load(jsonfile)
 
-with open('Block-Panchayat_dictionary.json') as jsonfile:
-	block_dict = json.load(jsonfile)
+with open('triples.json') as fp:
+	triplets = json.load(fp)
 
 
 def district_extractor():
@@ -28,33 +31,34 @@ def district_extractor():
 
 	element1 = WebDriverWait(driver, 10).until(
 		EC.presence_of_element_located((By.XPATH, '//*[@id="ContentPlaceHolder1_ddlState"]')))
-	dropdown1 = Select(driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_ddlState"]'))
+	dropdown1 = Select(element1)
 	dropdown1.select_by_visible_text('JHARKHAND')
 
 	district = str.upper(input('Enter name of district: '))
 
-	if district in district_dict.keys():
+	if district in triplets.keys():
 		element2 = WebDriverWait(driver, 10).until(
 			EC.presence_of_element_located((By.XPATH, '//*[@id="ContentPlaceHolder1_ddlDistrict"]')))
 		dropdown2 = Select(driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_ddlDistrict"]'))
 		dropdown2.select_by_visible_text(district)
 
-		for value in district_dict[district]:
-			print('Downloading from {} block'.format(value))
+		blocks = []
+		block_dict = triplets.get(district)
+		for key, value in block_dict.items():
+			blocks.append(key)
+
+		for block in blocks:
+			print('Downloading from {} block'.format(block))
 			element3 = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
 				(By.XPATH, '//*[@id="ContentPlaceHolder1_ddlBlock"]')))
-			dropdown3 = Select(driver.find_element_by_xpath(
-				'//*[@id="ContentPlaceHolder1_ddlBlock"]'))
-			dropdown3.select_by_visible_text(value)
-
-			new_key = value
-			for new_value in block_dict[new_key]:
-				print('--> {}'.format(new_value))
+			dropdown3 = Select(driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_ddlBlock"]'))
+			dropdown3.select_by_visible_text(block)
+			for panchayat in block_dict[block]:
+				print('--> {}'.format(panchayat))
 				element4 = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
 					(By.XPATH, '//*[@id="ContentPlaceHolder1_ddlPanchayat"]')))
-				dropdown4 = Select(driver.find_element_by_xpath(
-					'//*[@id="ContentPlaceHolder1_ddlPanchayat"]'))
-				dropdown4.select_by_visible_text(new_value)
+				dropdown4 = Select(driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_ddlPanchayat"]'))
+				dropdown4.select_by_visible_text(panchayat)
 				element5 = WebDriverWait(driver, 20).until(EC.presence_of_element_located(
 					(By.XPATH, '//*[@id="ContentPlaceHolder1_btnSubmit"]')))
 				actions = ActionChains(driver)
@@ -63,15 +67,13 @@ def district_extractor():
 				try:
 					element6 = WebDriverWait(driver, 20).until(EC.presence_of_element_located(
 						(By.XPATH, '//*[@id="ContentPlaceHolder1_gvData"]')))
-					table = driver.find_element_by_xpath(
-						'//*[@id="ContentPlaceHolder1_gvData"]')
+					table = driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_gvData"]')
 					rows = table.find_elements_by_tag_name('tr')
 					rows.pop(0)
 
 					data = []
 
 					for row in rows:
-						# print(row.text)
 						cells = row.find_elements_by_tag_name('td')
 						sl_no = cells[0].text
 						village = cells[1].text
@@ -85,8 +87,8 @@ def district_extractor():
 						amt_rlsd = cells[9].text
 						house_stat = cells[10].text
 
-						data.append([sl_no, village, reg_no, benf_name, f_m_name, h_alloted_to, sanc_no,
-						             sanc_amt, inst_paid, amt_rlsd, house_stat])
+						data.append([sl_no, village, reg_no, benf_name, f_m_name, h_alloted_to, sanc_no, sanc_amt,
+						             inst_paid, amt_rlsd, house_stat])
 
 						df = pd.DataFrame(data=data,
 						                  columns=['Sl_No', 'Village', 'Reg_No', 'Beneficiary_name',
@@ -94,10 +96,10 @@ def district_extractor():
 						                           'Sanction_No', 'Sanction_Amount', 'Installment_Paid',
 						                           'Amount_Released', 'House_Status'])
 
-						if not os.path.exists('data\{}'.format(value)):
-							os.makedirs('data\{}'.format(value))
+						if not os.path.exists('data\{}'.format(block)):
+							os.makedirs('data\{}'.format(block))
 
-						df.to_excel('data\{}\{}_{}.xlsx'.format(value, value, new_value))
+						df.to_excel('data\{}\{}_{}.xlsx'.format(block, block, panchayat))
 
 				except TimeoutException as ex:
 					print('-----> Data not found')
@@ -106,6 +108,7 @@ def district_extractor():
 					sleep(randint(1, 10))
 					dropdown4 = Select(driver.find_element_by_xpath(
 						'//*[@id="ContentPlaceHolder1_ddlPanchayat"]'))
+
 	else:
 		print("District name not found. Please try again")
 		district_extractor()
